6aedac2b25678614501fc3c0089b2641d0e44b0e
Bluebear233 <417350690@qq.com>
2016-05-19 22:11:10 +0800

    增加SD卡驱动并挂载

--- Applications/main.c
+++ Applications/main.c
@@ -14,12 +14,36 @@
 #include <dfs.h>
 #include <dfs_fs.h>
 #include <dfs_file.h>
+#include <dfs_posix.h>
 			
 
 int main(void)
 {
-	  if (dfs_mount("flash", "/", "elm", 0, 0) == 0)
-	      rt_kprintf("File System initialized!\n");
-	  else
-	      rt_kprintf("File System init failed!\n");
+	if (dfs_mount("flash", "/", "elm", 0, 0) != 0){
+		 rt_kprintf("flash File System init failed!\n");
+	     return 0;
+	}
+
+	rt_kprintf("flash File System initialized!\n");
+
+ 	DIR *dir = opendir("SD");
+ 	if(dir == RT_NULL){
+ 		rt_kprintf("mount dir not exis \n");
+ 		rt_kprintf("creating mount dir　\n");
+
+ 		if(mkdir("/SD",0x777) < 0){
+ 			rt_kprintf("creat mount dir failed \n");
+ 			return 0;
+ 		}
+
+ 	}else{
+ 		rt_kprintf("mount dir exis \n");
+ 		closedir(dir);
+ 	}
+
+	if (dfs_mount("sd0", "/SD", "elm", 0, 0) == 0)
+	     rt_kprintf("SD File System initialized!\n");
+	else
+	     rt_kprintf("SD File System init failed!\n");
+
 }
